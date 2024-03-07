e85a8678337d614c7c7432178122c600cb2f2d08
sunsunmonkey <2933201543@qq.com>
2024-03-07 09:31:34 +0800

    feat: clear the result data

--- src/layout/content/index.tsx
+++ src/layout/content/index.tsx
@@ -62,6 +62,7 @@ export const Content = () => {
             block
             onChange={(value) => {
               setEngine(value);
+              setPerfData({} as PerfData);
             }}
           ></Segmented>
         </Col>
